dd1312cdd6b7f2594ba39340a7633fe466f7ec1f
Hoda Shirzad <hoda@edwardrobertson.co.uk>
2018-03-28 10:57:34 +0100

    Fix bug with empty email address

--- src/config/bugonemail.php
+++ src/config/bugonemail.php
@@ -2,7 +2,7 @@
 
 return array(
     'project_name' => 'BugOnEmail!',
-    'notify_emails' => array(''),
+    'notify_emails' => array('your@email.com'),
     'email_template' => "bugonemail::email.notifyException",
     'notify_environment' => array('local'),
     'prevent_exception' => array('Symfony\Component\HttpKernel\Exception\NotFoundHttpException'),
